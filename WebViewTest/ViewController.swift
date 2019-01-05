3fbf7aeffa3a7097e5c6ec8d351802b593acb9f5
Raymund Tong <raymundtong@gmail.com>
2019-01-06 01:07:42 +0800

    Initial Commit

---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Raymund Tong on 6/1/2019.
+//  Copyright © 2019 Raymund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
